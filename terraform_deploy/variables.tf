6e74172865197dfca48dfe706a8c08b10def6cf6
VSUA <61660744+VSUA@users.noreply.github.com>
2020-11-23 17:55:37 +0200

    Create variables.tf file

---
+++ terraform_deploy/variables.tf
@@ -0,0 +1,11 @@
+variable "credentials" {
+  description = "Variable for github secret TF_VAR_CREDENTIALS"
+}
+
+variable "user" {
+  default = "root"
+}
+
+variable "project" {
+  default = "cosmic-slate-292412"
+}
